0928313baca8cf39c079ea915c273235c48b7fe6
pranto <prantochandrodas@gmail.com>
2024-04-27 12:12:37 +0600

    add filter feature

--- src/Components/SearchBus/SearchBus.jsx
+++ src/Components/SearchBus/SearchBus.jsx
@@ -1,14 +1,17 @@
 import { useQuery } from "react-query";
 import Loading from "../Loading/Loading";
 import AvailableBus from "../AvailableBus/AvailableBus";
+import { useForm } from "react-hook-form";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthProvider";
 
 
 const SearchBus = () => {
     const getText = localStorage.getItem('serch_text');
-
+    const [loading, setLoading] = useState(false);
     const searchValue = (JSON.parse(getText));
 
-    const { data: findbus = [searchValue.destination,searchValue.date,searchValue.bustype], isLoading } = useQuery({
+    const { data: findbus = [], isLoading, refetch } = useQuery({
         queryKey: ['searchBus'],
         queryFn: async () => {
             const res = await fetch(`http://localhost:3000/searchBus?destination=${searchValue.destination}&&date=${searchValue.date}&&bustype=${searchValue.bustype}`);
@@ -16,35 +19,52 @@ const SearchBus = () => {
             return data;
         }
     });
-    console.log(findbus)
-    if (isLoading) {
+
+
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+    const { findObj } = useContext(AuthContext);
+
+    const handleSearch = data => {
+        setLoading(true);
+        findObj["date"] = data.date;
+        findObj["destination"] = data.destination;
+        findObj["bustype"] = data.bustype;
+        localStorage.setItem('serch_text', JSON.stringify(findObj));
+        refetch();
+        window.location.reload();
+        setLoading(false);
+    }
+    if (isLoading || loading) {
         return <Loading />
     }
     return (
-        <div className="w-[90%] lg:flex md:flex mx-auto mt-20 justify-center">
-            <div className=" p-10 w-[350px] border border-gray-300 my-10">
-                <form action="">
+        <div className="w-[90%]  lg:flex md:flex mx-auto mt-20 justify-center">
+            <div className=" p-10 w-[350px] h-[400px] sticky top-0  border border-gray-300 my-10">
+                <form onSubmit={handleSubmit(handleSearch)}>
                     <div className="flex flex-col space-y-1">
                         <label htmlFor="date" className="text-sm font-semibold text-gray-500">Date</label>
-                        <input type="date" value={searchValue.date} className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                        <input name="date" type="date" {...register("date", { required: "date is required" })} value={searchValue.date} className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200" />
+                        {errors.date && <p className='text-red-600'>{errors.date?.message}</p>}
                     </div>
                     <div className="flex flex-col space-y-1 my-4">
                         <label htmlFor="email" className="text-sm font-semibold text-gray-500">Destination</label>
-                        <select name="" id="" className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200">
+                        <select name="destination" id="destination" {...register("destination", { required: "destination is required" })} className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200">
                             <option value={searchValue.destination}>{searchValue.destination}</option>
-                            {
-                                findbus?.map(bus => <option key={bus._id} className={bus?.destination == searchValue.destination ? 'hidden' : ''} value={bus?.destination}>{bus?.destination}</option>)
-                            }
+                            <option value="Dhaka to Feni" className={searchValue=='Dhaka to Feni'?'hidden':''}>Dhaka to Feni</option>
+                            <option value="Feni to Dhaka" className={searchValue=='Feni to Dhaka'?'hidden':''}>Feni to Dhaka</option>
+                            <option value="Chattrogram to Dhaka" className={searchValue=='Chattrogram to Dhaka'?'hidden':''}>Chattrogram to Dhaka</option>
+                            <option value="Dhaka to Chattrogram" className={searchValue=='Dhaka to Chattrogram'?'hidden':''}>Dhaka to Chattrogram</option>
                         </select>
+                        {errors.destination && <p className='text-red-600'>{errors.destination?.message}</p>}
                     </div>
                     <div className="flex flex-col space-y-1 my-4">
                         <label htmlFor="email" className="text-sm font-semibold text-gray-500">Bustype</label>
-                        <select name="" id="" className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200">
+                        <select name="bustype" id="bustype"{...register("bustype", { required: "bustype is required" })} className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200">
                             <option value={searchValue.bustype}>{searchValue.bustype}</option>
-                            {
-                                findbus?.map(bus => <option key={bus._id} className={bus?.bustype == searchValue.bustype ? 'hidden' : ''} value={bus?.bustype}>{bus?.bustype}</option>)
-                            }
+                            <option className={'AC' == searchValue.bustype ? 'hidden' : ''} value={'AC'}>{'AC'}</option>
+                            <option className={'NON AC' == searchValue.bustype ? 'hidden' : ''} value={'NON AC'}>{'NON AC'}</option>
                         </select>
+                        {errors.bustype && <p className='text-red-600'>{errors.bustype?.message}</p>}
                     </div>
 
                     <div className='flex items-center my-4 lg:mt-4 lg:my-0 md:my-0 lg:ml-[30px] '>
